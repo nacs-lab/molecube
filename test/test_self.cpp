557495c5352c390d06c2689c1837d4fd74b6c9e7
Yichao Yu <yyc1992@gmail.com>
2015-02-15 23:14:42 -0500

    [test]

--- test/test_self.cpp
+++ test/test_self.cpp
@@ -36,9 +36,11 @@ main()
     double fails[ndds] = {0.0};
     double runs[ndds] = {0.0};
     double fails2[ndds] = {0.0};
-    const constexpr int ncycle = 0x800;
+    const constexpr int ncycle = 0x100;
     bool dds_exists[ndds] = {};
     auto &pulser = Pulser::get_pulser();
+
+    auto prev_time = time(nullptr);
 
     for (int dds = 0;dds < ndds;dds++) {
         dds_exists[dds] = pulser.dds_exists(dds);
@@ -53,10 +55,16 @@ main()
                 continue;
             unsigned nfail = 0;
             unsigned nrun = 0;
-            test_dds(pulser, dds, 0x10000, 0x80, nfail, nrun);
+            test_dds(pulser, dds, 0x80000, 0x800, nfail, nrun);
             fails[dds] += double(nfail) / ncycle;
             runs[dds] += double(nrun) / ncycle;
             fails2[dds] += square(double(nfail)) / ncycle;
+        }
+        auto new_time = time(nullptr);
+        if (new_time > prev_time) {
+            prev_time = new_time;
+            printf("%d / %d (%.3f%%) done.\n",
+                   i + 1, ncycle, double(i + 1) / ncycle * 100);
         }
     }
     double total_fails = 0;
