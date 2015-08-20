f90915a0d79dd453516592f8305f313081c29ba9
Yichao Yu <yyc1992@gmail.com>
2015-08-20 14:12:16 -0400

    [test] update phy_addr test to use NaCs::getPhyAddr

--- test/test_phy_addr.cpp
+++ test/test_phy_addr.cpp
@@ -28,26 +28,38 @@ main()
     uint32_t page_size = getpagesize();
 
     void *virt_addr = mmap(nullptr, page_size, PROT_READ | PROT_WRITE,
-                           MAP_SHARED | MAP_ANONYMOUS | MAP_LOCKED, -1, 0);
-    auto virt_pfn = uintptr_t(virt_addr) / page_size;
-    int fd = open("/proc/self/pagemap", O_RDONLY);
-    uint64_t page_info;
-    pread(fd, &page_info, sizeof(page_info), virt_pfn * sizeof(uint64_t));
-    uint64_t page_info2;
-    pread(fd, &page_info2, sizeof(page_info2), virt_pfn * sizeof(uint64_t));
-    close(fd);
-    uint64_t phy_pfn = page_info & ((1ll << 55) - 1);
-    uint64_t phy_addr = phy_pfn * page_size;
+                           MAP_SHARED | MAP_ANONYMOUS | MAP_LOCKED |
+                           MAP_POPULATE, -1, 0);
+    void *phy_addr = getPhyAddr(virt_addr);
 
-    volatile void *virt_addr2 = mapFile("/dev/mem", off_t(phy_addr), page_size);
-    strcpy((char*)virt_addr, "random string");
-    printf("%s\n", virt_addr2);
+    void *virt_addr2 = mapFile("/dev/mem", off_t(phy_addr), page_size);
+    strcpy((char*)virt_addr2, "random string");
+    printf("%s\n", virt_addr);
+    strcpy((char*)virt_addr2, "random string22222");
+    printf("%s\n", virt_addr);
+
+    // strcpy((char*)virt_addr, "random string");
+    // printf("%s\n", virt_addr2);
+    // strcpy((char*)virt_addr, "random string22222");
+    // printf("%s\n", virt_addr2);
 
     std::cout << "page_size: 0x" << std::hex << page_size << std::endl;
     std::cout << "phy_addr : 0x" << std::hex << phy_addr << std::endl;
     std::cout << "virt_addr: 0x" << std::hex << virt_addr << std::endl;
-    std::cout << "page_info: 0x" << std::hex << page_info << std::endl;
-    std::cout << "virt_addr2: 0x" << std::hex << (void*)virt_addr2 << std::endl;
+    std::cout << "virt_addr2: 0x" << std::hex << virt_addr2 << std::endl;
+    std::cout << "phy_addr2: 0x" << std::hex
+              << getPhyAddr(virt_addr2) << std::endl;
+
+    *(volatile uint64_t*)virt_addr = 0;
+    msync(virt_addr, page_size, MS_SYNC);
+    msync(virt_addr2, page_size, MS_INVALIDATE);
+    std::cout << "mirrored int64: "
+              << *(volatile uint64_t*)virt_addr2 << std::endl;
+    *(volatile uint64_t*)virt_addr = 1;
+    msync(virt_addr, page_size, MS_SYNC);
+    msync(virt_addr2, page_size, MS_INVALIDATE);
+    std::cout << "mirrored int64: "
+              << *(volatile uint64_t*)virt_addr2 << std::endl;
 
     test_write(virt_addr, 1 << 25);
     test_write(virt_addr2, 1 << 25);
