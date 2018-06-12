078270df67c40c713678412f0c73ded48abce0b8
Yichao Yu <yyc1992@gmail.com>
2018-06-11 23:36:15 -0400

    [pulser] move the old FIFO class in

--- lib/pulser/controller.h
+++ lib/pulser/controller.h
@@ -48,6 +48,163 @@ struct Request {
     Request() = delete;
 };
 
+template<typename T, typename Lock=SpinLock>
+class FIFO {
+    // Not supported by stdc++ yet.
+    // static_assert(std::is_trivially_copyable<T>(), "");
+    FIFO(const FIFO&) = delete;
+    void operator=(const FIFO&) = delete;
+    // m_alloc is always a power of 2
+    size_t m_alloc;
+    T *m_buff;
+    mutable Lock m_lock;
+    size_t m_read_p; // always less than m_alloc
+    size_t m_write_p; // always less than m_alloc
+    inline void
+    doPush(const T *v)
+    {
+        // There has to be enough space in the buffer before calling this
+        // function (i.e. `spaceLeft() > 1`).
+        memcpy(m_buff + m_write_p, v, sizeof(T));
+        m_write_p = (m_write_p + 1) & (m_alloc - 1);
+    }
+    inline void
+    doPush(const T *v, size_t len)
+    {
+        // There has to be enough space in the buffer before calling this
+        // function (i.e. `spaceLeft() > len`).
+        const auto start_p = m_write_p;
+        m_write_p = (m_write_p + len) & (m_alloc - 1);
+        if (m_write_p > start_p || m_write_p == 0) {
+            memcpy(m_buff + start_p, v, len * sizeof(T));
+        } else {
+            const auto start_len = len - m_write_p;
+            memcpy(m_buff + start_p, v, start_len * sizeof(T));
+            memcpy(m_buff, v + start_len, m_write_p * sizeof(T));
+        }
+    }
+    inline void
+    doubleSize()
+    {
+        m_buff = (T*)realloc(m_buff, sizeof(T) * 2 * m_alloc);
+        if (m_read_p > m_write_p) {
+            if (m_write_p)
+                memcpy(m_buff + m_alloc, m_buff, m_write_p * sizeof(T));
+            m_write_p += m_alloc;
+        }
+        m_alloc *= 2;
+    }
+public:
+    inline
+    FIFO(size_t size=256)
+        : m_alloc(1 << max(getBits(size) - 1, 2)),
+          m_buff((T*)malloc(sizeof(T) * m_alloc)),
+          m_lock(),
+          m_read_p(0),
+          m_write_p(0)
+    {
+    }
+    inline size_t
+    capacity() const
+    {
+        return m_alloc;
+    }
+    template<bool lock=true>
+    inline size_t
+    size() const
+    {
+        CondLock<lock, Lock> locker(m_lock);
+        return (m_write_p - m_read_p) & (m_alloc - 1);
+    }
+    template<bool lock=true>
+    inline size_t
+    spaceLeft() const
+    {
+        CondLock<lock, Lock> locker(m_lock);
+        return capacity() - size<false>();
+    }
+    template<bool lock=true>
+    inline size_t
+    tryPush(const T *v, size_t len=1)
+    {
+        CondLock<lock, Lock> locker(m_lock);
+        len = min(spaceLeft<false>() - 1, len);
+        if (len > 0)
+            doPush(v, len);
+        return len;
+    }
+    template<bool lock=true>
+    inline bool
+    tryPush(const T &v)
+    {
+        CondLock<lock, Lock> locker(m_lock);
+        if (spaceLeft<false>() <= 1)
+            return false;
+        doPush(&v);
+        return true;
+    }
+    template<bool lock=true>
+    inline void
+    push(const T *v, size_t len=1)
+    {
+        CondLock<lock, Lock> locker(m_lock);
+        while (spaceLeft<false>() <= len) {
+            doubleSize();
+        }
+        doPush(v, len);
+    }
+    template<bool lock=true>
+    inline void
+    push(const T &v)
+    {
+        CondLock<lock, Lock> locker(m_lock);
+        if (spaceLeft<false>() <= 1) {
+            doubleSize();
+        }
+        doPush(&v);
+    }
+    template<bool lock=true>
+    inline T
+    pop()
+    {
+        // This function has no protection for overflowing
+        CondLock<lock, Lock> locker(m_lock);
+        T v = m_buff[m_read_p];
+        m_read_p = (m_read_p + 1) & (m_alloc - 1);
+        return v;
+    }
+    template<bool lock=true>
+    inline size_t
+    pop(T *v, size_t len=1)
+    {
+        CondLock<lock, Lock> locker(m_lock);
+        len = min(size<false>(), len);
+        if (!len)
+            return 0;
+        const auto start_p = m_read_p;
+        m_read_p = (m_read_p + len) & (m_alloc - 1);
+
+        if (m_read_p > start_p || m_read_p == 0) {
+            memcpy(v, m_buff + start_p, len * sizeof(T));
+        } else {
+            const auto start_len = len - m_read_p;
+            memcpy(v, m_buff + start_p, start_len * sizeof(T));
+            memcpy(v + start_len, m_buff, m_read_p * sizeof(T));
+        }
+        return len;
+    }
+    inline size_t
+    tryPop(T *v, size_t len=1)
+    {
+        std::unique_lock<Lock> locker(m_lock, std::defer_lock);
+        if (!locker.try_lock()) {
+            CPU::pause();
+            return 0;
+        }
+        return pop<false>(v, len);
+    }
+};
+
 /**
  * This is the object that manages the threads that talks to the FPGA.
  */
---
+++ test/pulser/test_fifo.cpp
@@ -0,0 +1,229 @@
+/*************************************************************************
+ *   Copyright (c) 2015 - 2015 Yichao Yu <yyc1992@gmail.com>             *
+ *                                                                       *
+ *   This library is free software; you can redistribute it and/or       *
+ *   modify it under the terms of the GNU Lesser General Public          *
+ *   License as published by the Free Software Foundation; either        *
+ *   version 3.0 of the License, or (at your option) any later version.  *
+ *                                                                       *
+ *   This library is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU    *
+ *   Lesser General Public License for more details.                     *
+ *                                                                       *
+ *   You should have received a copy of the GNU Lesser General Public    *
+ *   License along with this library. If not,                            *
+ *   see <http://www.gnu.org/licenses/>.                                 *
+ *************************************************************************/
+
+#include <nacs-utils/timer.h>
+#include <nacs-utils/container.h>
+
+#include <mutex>
+#include <atomic>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+#ifdef NDEBUG
+#  undef NDEBUG
+#endif
+
+#include <assert.h>
+
+static constexpr size_t N = 1000000;
+
+using namespace NaCs;
+using namespace std::literals;
+
+template<size_t sz, typename F>
+static inline void
+fifo_list_try_pusher(F &fifo)
+{
+    std::this_thread::yield();
+    size_t buff[sz];
+    for (size_t i = 0;i < N;) {
+        auto end = min(sz, N - i);
+        for (size_t j = 0;j < end;j++) {
+            buff[j] = i + j;
+        }
+        size_t pushed;
+        while ((pushed = fifo.tryPush(buff, 1)) == 0) {
+            std::this_thread::yield();
+        }
+        i += pushed;
+    }
+}
+
+template<size_t sz, typename F>
+static inline void
+fifo_list_pusher(F &fifo)
+{
+    std::this_thread::yield();
+    size_t buff[sz];
+    for (size_t i = 0;i < N;) {
+        auto end = min(sz, N - i);
+        for (size_t j = 0;j < end;j++) {
+            buff[j] = i + j;
+        }
+        fifo.push(buff, end);
+        i += end;
+    }
+}
+
+template<size_t sz, typename F>
+static inline void
+fifo_poper(F &fifo)
+{
+    size_t buff[sz];
+    for (size_t i = 0;i < N;) {
+        size_t size;
+        while (!(size = fifo.pop(buff, sz)))
+            std::this_thread::yield();
+        for (size_t j = 0;j < size;j++) {
+            assert(buff[j] == i + j);
+        }
+        i += size;
+    }
+}
+
+template<size_t sz, typename F>
+static inline void
+fifo_try_poper(F &fifo)
+{
+    size_t buff[sz];
+    for (size_t i = 0;i < N;) {
+        size_t size;
+        while (!(size = fifo.tryPop(buff, sz)))
+            std::this_thread::yield();
+        for (size_t j = 0;j < size;j++) {
+            assert(buff[j] == i + j);
+        }
+        i += size;
+    }
+}
+
+template<typename Lock, size_t init_size=256>
+static inline void
+test_fifo()
+{
+    FIFO<size_t, Lock> fifo(init_size);
+    assert(fifo.size() == 0);
+    assert(fifo.capacity() == init_size);
+    assert(fifo.spaceLeft() == init_size);
+
+    Timer timer;
+    std::thread t1([&] {
+            std::this_thread::yield();
+            for (size_t i = 0;i < N;i++) {
+                while (!fifo.tryPush(i)) {
+                    std::this_thread::yield();
+                }
+            }
+        });
+    for (size_t i = 0;i < N;i++) {
+        while (!fifo.size()) {
+            std::this_thread::yield();
+        }
+        auto res = fifo.pop();
+        assert(res == i);
+    }
+    t1.join();
+    auto write_time = timer.elapsed();
+
+    std::cout << "Average time per write: "
+              << double(write_time) / double(N) / 1e3 << " us"
+              << std::endl;
+
+    timer.restart();
+    std::thread t3([&] {
+            fifo_list_try_pusher<1>(fifo);
+        });
+    fifo_poper<3>(fifo);
+    t3.join();
+    auto write_time3 = timer.elapsed();
+
+    std::cout << "Average time per write: "
+              << double(write_time3) / double(N) / 1e3 << " us"
+              << std::endl;
+
+    timer.restart();
+    std::thread t4([&] {
+            fifo_list_try_pusher<3>(fifo);
+        });
+    fifo_poper<1>(fifo);
+    t4.join();
+    auto write_time4 = timer.elapsed();
+
+    std::cout << "Average time per write: "
+              << double(write_time4) / double(N) / 1e3 << " us"
+              << std::endl;
+
+    assert(fifo.capacity() == init_size);
+
+    timer.restart();
+    std::thread t2([&] {
+            std::this_thread::yield();
+            for (size_t i = 0;i < N;i++) {
+                fifo.push(i);
+            }
+        });
+    size_t i = 0;
+    while (i < N) {
+        size_t size = fifo.size();
+        if (!size) {
+            std::this_thread::yield();
+            continue;
+        }
+        for (size_t j = 0;j < size;j++) {
+            assert(fifo.pop() == i++);
+        }
+    }
+    t2.join();
+    auto write_time2 = timer.elapsed();
+
+    std::cout << "Average time per write: "
+              << double(write_time2) / double(N) / 1e3 << " us"
+              << std::endl;
+    std::cout << "Total size: " << fifo.capacity() << std::endl;
+
+    timer.restart();
+    std::thread t5([&] {
+            fifo_list_pusher<1>(fifo);
+        });
+    fifo_try_poper<3>(fifo);
+    t5.join();
+    auto write_time5 = timer.elapsed();
+
+    std::cout << "Average time per write: "
+              << double(write_time5) / double(N) / 1e3 << " us"
+              << std::endl;
+    std::cout << "Total size: " << fifo.capacity() << std::endl;
+
+    timer.restart();
+    std::thread t6([&] {
+            fifo_list_pusher<3>(fifo);
+        });
+    fifo_try_poper<1>(fifo);
+    t6.join();
+    auto write_time6 = timer.elapsed();
+    std::cout << "Average time per write: "
+              << double(write_time6) / double(N) / 1e3 << " us"
+              << std::endl;
+    std::cout << "Total size: " << fifo.capacity() << std::endl;
+}
+
+int
+main()
+{
+    std::cout << "<SpinLock, 256>: " << std::endl;
+    test_fifo<SpinLock, 256>();
+    std::cout << "<SpinLock, 8192>: " << std::endl;
+    test_fifo<SpinLock, 8192>();
+    std::cout << "<std::mutex, 256>: " << std::endl;
+    test_fifo<std::mutex, 256>();
+    std::cout << "<std::mutex, 8192>: " << std::endl;
+    test_fifo<std::mutex, 8192>();
+    return 0;
+}
